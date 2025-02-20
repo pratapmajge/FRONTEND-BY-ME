48ad145e59190b1850585b88c546f9044dfb3d7e
pratapmajge <majgepratap1504@gmail.com>
2025-02-20 23:47:52 +0530

    commit 3

---
+++ src/folders/data.js
@@ -0,0 +1,28 @@
+export const itemData =[{
+    item_img: "/assets/Bonda.jpg"
+},
+{
+    item_img: "/assets/Dosa.jpg"
+},
+{
+    item_img: "/assets/Idli.jpg"
+},
+{
+    item_img: "/assets/Juice.jpg"
+},
+{
+    item_img: "/assets/Pancake.jpg"
+},
+{
+    item_img: "/assets/Paratha.jpg"
+},
+{
+    item_img: "/assets/Poha.jpg"
+},
+{
+    item_img: "/assets/Poori.jpg"
+},
+{
+    item_img: "/assets/Vada.jpg"
+
+},]
